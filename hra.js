965ebb23a4e592842a95f6bc46af1896d5988cc6
Pavla Taterová <taterova.pavla@gmail.com>
2023-10-24 21:33:50 +0200

    Uprava hra.js

--- hra.js
+++ hra.js
@@ -9,56 +9,44 @@ crossElement.style.display = "none";
 
 const ButtonsElement = document.querySelectorAll(".game__field");
 
-const changeClass = (event) => {
+const nextMove = (event) => {
   if (currentPlayer === "circle") {
     event.target.classList.add("game__field--circle");
     event.target.disabled = true;
-  } else {
-    event.target.classList.add("game__field--cross");
-    event.target.disabled = true;
-  }
-};
-
-const changePlayer = () => {
-  if (currentPlayer === "circle") {
     currentPlayer = "cross";
-  } else {
-    currentPlayer = "circle";
-  }
-};
-
-const changeNextPlayer = () => {
-  if (currentPlayer === "circle") {
     circleElement.style.display = "block";
     crossElement.style.display = "none";
-  } else {
+  } else if (currentPlayer === "cross") {
+    event.target.classList.add("game__field--cross");
+    event.target.disabled = true;
+    currentPlayer = "circle";
     circleElement.style.display = "none";
     crossElement.style.display = "block";
   }
 };
 
-const checkWinner = () => {
-  const herniPole = [];
+const fillGameField = () => {
+  const gameField = [];
   ButtonsElement.forEach((button) => {
     if (button.classList.contains("game__field--circle")) {
-      herniPole.push("o");
+      gameField.push("o");
     } else if (button.classList.contains("game__field--cross")) {
-      herniPole.push("x");
+      gameField.push("x");
     } else {
-      herniPole.push("_");
+      gameField.push("_");
     }
   });
 
-  const vitez = findWinner(herniPole);
+  const vitez = findWinner(gameField);
   if (vitez === "o" || vitez === "x") {
-    alert(`Vyhrál hráč se symbolem ${vitez}.`); // Vyhrál hráč se symbolem o.
-    location.reload();
+    setTimeout(() => {
+      alert(`Vyhrál hráč se symbolem ${vitez}.`), // Vyhrál hráč se symbolem o.
+        location.reload();
+    }, 300);
   }
 };
 
 ButtonsElement.forEach((button) => {
-  button.addEventListener("click", changeClass);
-  button.addEventListener("click", changePlayer);
-  button.addEventListener("click", changeNextPlayer);
-  button.addEventListener("click", checkWinner);
+  button.addEventListener("click", nextMove);
+  button.addEventListener("click", fillGameField);
 });
