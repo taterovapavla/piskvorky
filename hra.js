390cd2e72dfae848c2666d3ec95bc95a8877c727
Pavla Taterová <taterova.pavla@gmail.com>
2023-10-24 22:39:50 +0200

    Přidání funkce aiPlays

--- hra.js
+++ hra.js
@@ -13,16 +13,50 @@ const nextMove = (event) => {
   if (currentPlayer === "circle") {
     event.target.classList.add("game__field--circle");
     event.target.disabled = true;
-    currentPlayer = "cross";
     circleElement.style.display = "block";
     crossElement.style.display = "none";
+    currentPlayer = "cross";
   } else if (currentPlayer === "cross") {
     event.target.classList.add("game__field--cross");
     event.target.disabled = true;
-    currentPlayer = "circle";
     circleElement.style.display = "none";
     crossElement.style.display = "block";
+    currentPlayer = "circle";
   }
+};
+
+const aiPlays = async (gameField) => {
+  const fields = document.querySelectorAll(".game__field");
+  fields.forEach((field) => {
+    field.disabled = true;
+  });
+  const response = await fetch(
+    "https://piskvorky.czechitas-podklady.cz/api/suggest-next-move",
+    {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        board: gameField,
+        player: "x", // Hledá tah pro křížek.
+      }),
+    }
+  );
+  const data = await response.json();
+  fields.forEach((field) => {
+    if (
+      field.classList.contains("game__field--circle") ||
+      field.classList.contains("game__field--cross")
+    ) {
+      field.disabled = true;
+    } else {
+      field.disabled = false;
+    }
+  });
+  const { x, y } = data.position; // x bude 0 a y bude 1, protože to je jediné volné políčko. x 0 odpovídá prvnímu sloupci a y 1 druhému řádku.
+  const field = fields[x + y * 10]; // Najde políčko na příslušné pozici.
+  field.click(); // Simuluje kliknutí. Spustí událost `click` na políčku.
 };
 
 const fillGameField = () => {
@@ -36,6 +70,10 @@ const fillGameField = () => {
       gameField.push("_");
     }
   });
+
+  if (currentPlayer === "cross") {
+    aiPlays(gameField);
+  }
 
   const vitez = findWinner(gameField);
   if (vitez === "o" || vitez === "x") {
